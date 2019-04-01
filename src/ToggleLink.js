dcc96887d0353c61e5a9535889ca219a64587e37
Tyree Barron <tbarron@crossmw.com>
2019-04-01 15:47:23 -0400

    added better link toggling as well as cart feature

--- src/ToggleLink.js
+++ src/ToggleLink.js
@@ -1,4 +1,4 @@
-import React, { Component } from "react";
+import React from "react";
 import { Route, Link } from "react-router-dom";
 
 // Made a functional component instead of a class component.
---
+++ src/shop/CartSummary.js
@@ -0,0 +1,30 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+export const CartSummary = props => {
+    const getSummary = () => {
+        if (props.cartItems > 0) {
+            return <span>
+                {props.cartItems} item(s),
+                ${props.cartPrice.toFixed(2)}
+            </span>
+
+        } else {
+            return <span>Your cart: (empty) </span>
+        }
+    }
+    const getLinkClasses = () => {
+        return `btn btn-sm bg-dark text-white
+    ${ props.cartItems === 0 ? "disabled" : ""}`;
+    }
+
+    return <div className="float-right">
+        <small>
+            {getSummary()}
+            <Link className={getLinkClasses()}
+                to="/shop/cart">
+                <i className="fa fa-shopping-cart"></i>
+            </Link>
+        </small>
+    </div>
+}
